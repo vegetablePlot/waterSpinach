1c822321ea533cd95f559652baef14ab0625ec0e
liqiang <123456>
2023-02-22 11:36:58 +0800

    初始化

---
+++ lantian-server/src/main/java/com/vegetablePlot/lantian/server/LantianServerApplication.java
@@ -0,0 +1,13 @@
+package com.vegetablePlot.lantian.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LantianServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LantianServerApplication.class, args);
+	}
+
+}
